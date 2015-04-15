0560a32334d1259a2f49c48229c473c257ead0ec
Brian Capouch <brianc@saintjoe.edu>
2015-04-15 15:47:54 -0400

    Add new broken code

---
+++ Attr/src/edu/saintjoe/cs/brianc/attr/AttrDriver.java
@@ -0,0 +1,12 @@
+package edu.saintjoe.cs.brianc.attr;
+
+public class AttrDriver {
+	public Attr one;
+	public Attr two;
+	public Attr three;
+	
+	public static void main(String[] args) {
+		
+	one = new Attr("Name", "Brian");
+	}
+}
