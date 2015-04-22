3e999339bad7cfa9554596f47073ee1b45136c6d
Brian Capouch <brianc@saintjoe.edu>
2015-04-22 15:30:37 -0400

    Example driver class

--- Attr/src/edu/saintjoe/cs/brianc/attr/AttrDriver.java
+++ Attr/src/edu/saintjoe/cs/brianc/attr/AttrDriver.java
@@ -1,12 +1,18 @@
 package edu.saintjoe.cs.brianc.attr;
 
 public class AttrDriver {
-	public Attr one;
-	public Attr two;
-	public Attr three;
-	
+
 	public static void main(String[] args) {
 		
+	Attr one;
+	Attr two;
+	Attr three;
+		
 	one = new Attr("Name", "Brian");
-	}
+	two = new Attr("Height", 60);
+	three = new Attr("Mystery");
+	
+	System.out.println(one.toString());
+	System.out.println(two.toString());
+	};
 }
