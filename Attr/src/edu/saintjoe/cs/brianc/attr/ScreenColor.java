c25b3348c2a7e39a1eb276196c8652b632c31689
Brian Capouch <brianc@saintjoe.edu>
2015-04-22 03:37:47 -0400

    Clean up

--- Attr/src/edu/saintjoe/cs/brianc/attr/ScreenColor.java
+++ Attr/src/edu/saintjoe/cs/brianc/attr/ScreenColor.java
@@ -2,20 +2,21 @@ package edu.saintjoe.cs.brianc.attr;
 
 public class ScreenColor {
 	
-	// A ScreenColor object has to properties (data members)
+	// A ScreenColor object has two fields (data members)
 	private int colorValue;
 	private String description;
 
-	  public ScreenColor(Object colorDescription) {
-	    description = (String) colorDescription;
+	
+	public ScreenColor(Object colorDescription) {
+		description = (String) colorDescription;
 	    colorValue = encode(colorDescription);
 	    }
 	  
 	  private int encode(Object colorDescription) {
 		  if (colorDescription.equals("transparent"))
-				  return (colorValue = 0x0000000);
+				  return (colorValue = 0x000000);
 		  else if (colorDescription.equals("red"))
-			  return (colorValue = 0x1231232);
+			  return (colorValue = 0xff0000);
 		  return 0;
 	  }
 
