0d962e3b316c81db913ec913c49cf156c6333439
Brian Capouch <brianc@saintjoe.edu>
2015-04-24 15:50:22 -0400

    Begin working on a driver for ColorAttr

---
+++ Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
@@ -0,0 +1,14 @@
+package edu.saintjoe.cs.brianc.attr;
+
+public class ColorAttrDriver {
+	
+	public static void main(String[] args ) {
+		
+		ColorAttr testColor;
+		
+		testColor = new ColorAttr("red");
+		System.out.println(testColor.toString());
+	
+	
+}
+}
