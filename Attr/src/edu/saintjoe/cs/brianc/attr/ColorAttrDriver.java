536639d861e6799090bf0b11a0e6eafadf6e799b
brianc <brianc@localhost>
2015-04-27 15:52:03 -0400

    Add more details to driver for ColorAttr

--- Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
+++ Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
@@ -5,9 +5,13 @@ public class ColorAttrDriver {
 	public static void main(String[] args ) {
 		
 		ColorAttr testColor;
+		ColorAttr colorTwo;
+		ScreenColor newColor = new ScreenColor("green");
 		
 		testColor = new ColorAttr("Color","red");
 		System.out.println(testColor.toString());
+		colorTwo = new ColorAttr("green", newColor);
+		
 	
 	
 }
